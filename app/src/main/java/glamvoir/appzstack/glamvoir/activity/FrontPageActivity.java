dda6476e26d7d804792f3af287df7929e6b5155b
Jai <jaimishra54@gmail.com>
2015-08-24 23:06:28 +0530

    4

--- app/src/main/java/glamvoir/appzstack/glamvoir/activity/FrontPageActivity.java
+++ app/src/main/java/glamvoir/appzstack/glamvoir/activity/FrontPageActivity.java
@@ -480,7 +480,7 @@ public class FrontPageActivity extends AppCompatActivity implements View.OnClick
 
                     @Override
                     public void onError(FacebookException error) {
-                        //  Log.ddddd(TAG_ERROR,error.toString());
+                        //  Log.dd(TAG_ERROR,error.toString());
                     }
                 });
     }
