b9c48a4c90457f50e66ece758b720788a7e4d216
Jai <jaimishra54@gmail.com>
2015-08-24 22:59:22 +0530

    2

--- app/src/main/java/glamvoir/appzstack/glamvoir/activity/FrontPageActivity.java
+++ app/src/main/java/glamvoir/appzstack/glamvoir/activity/FrontPageActivity.java
@@ -475,7 +475,7 @@ public class FrontPageActivity extends AppCompatActivity implements View.OnClick
 
                     @Override
                     public void onCancel() {
-                        // tr.ssdd(TAG_CANCEL,"On cancel");
+                        // tr.sssdd(TAG_CANCEL,"On cancel");
                     }
 
                     @Override
