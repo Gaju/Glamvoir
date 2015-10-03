cb5f0871a54c085b73d2d06135a27d587df078b9
Jai <jaimishra54@gmail.com>
2015-10-03 22:52:12 +0530

    upload edit option

--- app/src/main/java/glamvoir/appzstack/glamvoir/activity/MysaveActivity.java
+++ app/src/main/java/glamvoir/appzstack/glamvoir/activity/MysaveActivity.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -72,5 +75,34 @@ public class MysaveActivity extends FFSPActivity {
             e.printStackTrace();
         }
         return newIntent;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mysave_menu, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.edit) {
+
+            Toast.makeText(getApplicationContext(), "EDIT CLICKED",
+                    Toast.LENGTH_SHORT).show();
+            canDelete();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void canDelete() {
+
+        Toast.makeText(getApplicationContext(), "Happy",
+                Toast.LENGTH_SHORT).show();
+
     }
 }
