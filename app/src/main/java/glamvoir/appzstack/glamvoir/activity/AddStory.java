a0ad689ab48de4138638fae12423ec732f8b7630
Jai <jaimishra54@gmail.com>
2015-10-26 00:08:25 +0530

    calender

--- app/src/main/java/glamvoir/appzstack/glamvoir/activity/AddStory.java
+++ app/src/main/java/glamvoir/appzstack/glamvoir/activity/AddStory.java
@@ -1,11 +1,14 @@
 package glamvoir.appzstack.glamvoir.activity;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -13,11 +16,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.DatePicker;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -30,7 +35,11 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import glamvoir.appzstack.glamvoir.R;
 import glamvoir.appzstack.glamvoir.adapter.Action;
@@ -53,6 +62,11 @@ public class AddStory extends AppCompatActivity implements
      }*/
     private static final String TAG = "AddStory";
 
+    // Variable for storing current date and time
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    String userCurrentDate;
+    String userCooseDate,finalDate;
+
     GridView gridGallery;
     Handler handler;
     GalleryAdapter adapter;
@@ -61,13 +75,13 @@ public class AddStory extends AppCompatActivity implements
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
 
-
+    String result=null;
     private RequestBean mRequestBean;
     private Toolbar toolbar;
     Spinner dropDownMenu;
     TextView selected_text;
     private LinearLayout lltool;
-    ImageButton galleryImages;
+    ImageButton galleryImages,time_calender,calender_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,8 +113,16 @@ public class AddStory extends AppCompatActivity implements
 
         getToolbar(toolbar);
         addItemsToSpinner();
+        CurrentDate();
 
         //    init();
+    }
+
+    private void CurrentDate() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+        userCurrentDate = df.format(c.getTime());
+        Toast.makeText(this, userCurrentDate, Toast.LENGTH_SHORT).show();
     }
 
 
@@ -248,7 +270,8 @@ public class AddStory extends AppCompatActivity implements
      */
     private void initListener() {
         galleryImages.setOnClickListener(this);
-
+        calender_item.setOnClickListener(this);
+        time_calender.setOnClickListener(this);
     }
 
 
@@ -263,6 +286,8 @@ public class AddStory extends AppCompatActivity implements
         dropDownMenu = (Spinner) findViewById(R.id.spinner_nav);
         lltool.setVisibility(View.VISIBLE);
         selected_text = (TextView) findViewById(R.id.selected_text);
+        calender_item=(ImageButton)findViewById(R.id.calender_item);
+        time_calender=(ImageButton)findViewById(R.id.time_calender);
         if (bundle != null) {
             selected_text.setVisibility(View.VISIBLE);
             selected_text.setText(bundle.getString("CATOGERYNAME"));
@@ -331,6 +356,50 @@ public class AddStory extends AppCompatActivity implements
                 i.setAction(Action.ACTION_MULTIPLE_PICK);
                 startActivityForResult(i, 200);
                 break;
+            case R.id.calender_item:
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dpd = new DatePickerDialog(this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                Toast.makeText(AddStory.this, dayOfMonth + "-" + (monthOfYear + 1) + "-" + year, Toast.LENGTH_LONG).show();
+                                userCooseDate = year+ "/" + (monthOfYear + 1) + "/" + dayOfMonth;
+                                DataComprision(userCooseDate,userCurrentDate);
+                                if (result.equals("1")||result.equals("0")){
+                                    finalDate=year+ "/" + (monthOfYear + 1) + "/" + dayOfMonth;
+                                }
+                                else {
+                                    Toast.makeText(AddStory.this,"Please give current and future event date",Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        }, mYear, mMonth, mDay);
+                dpd.show();
+
+                break;
+            case R.id.time_calender:
+                TimePickerDialog tpd = new TimePickerDialog(this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                Toast.makeText(AddStory.this, hourOfDay + ":" + minute, Toast.LENGTH_LONG).show();
+
+
+                            }
+                        }, mHour, mMinute, false);
+                tpd.show();
+                break;
+
         }
     }
 
@@ -367,4 +436,40 @@ public class AddStory extends AppCompatActivity implements
             adapter.addAll(dataT);
         }
     }
+
+   public String DataComprision(String userCooseDate, String userCurrentDate) {
+
+       try{
+
+           SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+           Date date1 = sdf.parse(userCooseDate);
+           Date date2 = sdf.parse(userCurrentDate);
+
+           System.out.println(sdf.format(date1));
+           System.out.println(sdf.format(date2));
+
+           if(date1.after(date2)){
+               System.out.println("Date1 is after Date2");
+               result="1";
+
+           }
+
+           if(date1.before(date2)){
+               System.out.println("Date1 is before Date2");
+               result="-1";
+           }
+
+           if(date1.equals(date2)){
+               System.out.println("Date1 is equal Date2");
+               result="0";
+           }
+
+       }catch( ParseException ex){
+           ex.printStackTrace();
+       }
+
+       return result;
+   }
+
 }
